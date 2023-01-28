53364070ddd1597f32d920bb11d68989ffa709fb
Papai <papaimistry01@gmail.com>
2023-01-28 12:00:53 +0530

    second commit

--- myonline.py
+++ myonline.py
@@ -7,5 +7,5 @@ updater = Updater(token='5837129083:AAHuwFCnUcX1R9KZyItA7ZDnRgacjm81rEY', use_co
 updater.start_webhook(listen="0.0.0.0",
                       port=YOUR_PORT,
                       url_path='5837129083:AAHuwFCnUcX1R9KZyItA7ZDnRgacjm81rEY')
-updater.bot.setWebhook(url='YOUR_WEBHOOK_URL' + '5837129083:AAHuwFCnUcX1R9KZyItA7ZDnRgacjm81rEY')
+updater.bot.setWebhook(url='https://telegram-bot-1ddq.onrender.com' + '5837129083:AAHuwFCnUcX1R9KZyItA7ZDnRgacjm81rEY')
 updater.idle()
