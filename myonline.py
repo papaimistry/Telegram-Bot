3cf34a2152f97f39fb4beaf4d433f1cdb2d72256
Papai <papaimistry01@gmail.com>
2023-01-28 12:25:49 +0530

    forth commit

--- myonline.py
+++ myonline.py
@@ -5,7 +5,9 @@ bot = telegram.Bot(token='5837129083:AAHuwFCnUcX1R9KZyItA7ZDnRgacjm81rEY')
 # Use webhook to always keep the bot online
 updater = Updater(token='5837129083:AAHuwFCnUcX1R9KZyItA7ZDnRgacjm81rEY', use_context=True)
 updater.start_webhook(listen="0.0.0.0",
-                      port= 3001,
+                      port= 13.228.225.19
+18.142.128.26
+54.254.162.138,
                       url_path='5837129083:AAHuwFCnUcX1R9KZyItA7ZDnRgacjm81rEY')
 updater.bot.setWebhook(url='https://telegram-bot-1ddq.onrender.com' + '5837129083:AAHuwFCnUcX1R9KZyItA7ZDnRgacjm81rEY')
 updater.idle()
